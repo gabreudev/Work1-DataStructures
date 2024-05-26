7909aeb7dfff1fe04caaa2515b6e055eedf897ad
Alef Cauan Sousa Rodriguês <149737667+alefCauan@users.noreply.github.com>
2024-05-26 13:52:15 -0300

    feat: RGB and GRAY (min_cdf, cumulative histogram, histogram_tile, histogram_equalizer, clahe, sum_kernel, median_blur)

--- image.h
+++ image.h
@@ -23,12 +23,22 @@ typedef struct imageRGB {
     PixelRGB *pixels;
 } ImageRGB;
 
+
+
 // Funções de criação e liberação
 ImageGray *create_image_gray(int largura, int altura);
 void free_image_gray(ImageGray *image);
+void free_pixel_gray(PixelGray *pixel);
 
 ImageRGB *create_image_rgb(int largura, int altura);
 void free_image_rgb(ImageRGB *image);
+void free_pixel_RGB(PixelRGB *pixel);
+
+// Operações de arquivos 
+FILE *open(char *name, char *operation);
+
+// Operações de ERRO
+void check_allocation(void *pointer, const char *mensage);
 
 // Operações para ImageGray
 ImageGray *flip_vertical_gray(ImageGray *image);
@@ -41,11 +51,47 @@ ImageRGB *flip_horizontal_rgb(const ImageRGB *image);
 ImageRGB *transpose_rgb(const ImageRGB *image);
 
 // Manipulação por pixel para ImageGray
+
+// AUXILIARES
+/// Procura o Valor minimo do CDF maior que zero
+int min_cdf(int *CDF);
+// Calcula a soma cumulativa dos valores do histograma
+int *cumulative_histogram(int *histogram);
+
+/////////////////
+
+// Calcula cada tile individual do histograma 
+void histogram_tile_gray(const ImageGray *image, int *histogram, int x1, int x2, int tile_width, int tile_height);
+
+// Recebe o CDF e calcula o novo valor do pixel para a imagem
+void histogram_equalizer_gray(ImageGray *image, int *CDF, int tile_width, int tile_height, int tile_increasew, int tile_increaseh);
+
+// Aplica o clahe, retornando a imagem equalizada 
 ImageGray *clahe_gray(const ImageGray *image, int tile_width, int tile_height);
+
+// Calcula a soma dos tiles ao redor do central
+int soma_kernel_gray(const ImageGray *image, int index_i, int index_j, int kernel);
+
+// Substitui cada pixel pela média dos pixels em sua vizinhança
 ImageGray *median_blur_gray(const ImageGray *image, int kernel_size);
 
 // Manipulação por pixel para ImageRGB
+// RGB: Calcula a soma cumulativa dos valores do histograma
+PixelRGB *cumulative_histogram_rgb(PixelRGB *histogram);
+
+// RGB: Calcula cada tile individual do histograma 
+void histogram_tile_rgb(const ImageRGB *image, PixelRGB *histogram, int x1, int x2, int tile_width, int tile_height);
+
+// RGB: Recebe o CDF e calcula o novo valor do pixel para a imagem
+void equalize_tile_rgb(ImageRGB *image, PixelRGB *CDF, int tile_width, int tile_height, int tile_increasew, int tile_increaseh);
+
+// RGB: Aplica o clahe, retornando a imagem equalizada 
 ImageRGB *clahe_rgb(const ImageRGB *image, int tile_width, int tile_height);
-ImageRGB *median_blur_rgb(const ImageRGB *image, int kernel_size);
+
+// RGB: Calcula a soma dos tiles ao redor do central
+PixelRGB *soma_kernel_RGB(const ImageRGB *image, int index_i, int index_j, int kernel);
+
+// RGB: substitui cada pixel pela média dos pixels em sua vizinhança
+ImageRGB *median_blur_RGB(const ImageRGB *image, int kernel_size);
 
 #endif // IMAGE_H
